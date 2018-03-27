8f71ae576cbf18554358888548e82f39fb0c4bf9
Guillaume GREGOIRE <ggregoir@e3r2p19.42.fr>
2018-03-27 22:45:17 +0200

    deleted

--- j01/ex03/ft_split.php
+++
@@ -1,9 +0,0 @@
-<?php
-function ft_split($variable)
-{
-	$my_array = explode(" ", $variable);
-	$my_array = array_filter($my_array);
-	sort($my_array);
-	return($my_array);
-}
-?>
--- j01/ex07/rostring.php
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/php
-<?php
-$array = array_filter(explode(" ", $argv[1]));
-$last = $array[0];
-unset($array[0]);
-foreach ($array as $elem) 
-	echo "$elem ";
-echo "$last\n";
-?>
